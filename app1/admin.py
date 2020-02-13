73fc53a4313d69afd0abe73707422485eeab9456
VovaBobyr <vb.subscribe@gmail.com>
2020-02-13 23:31:16 +0100

    Inicial creation

---
+++ app1/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app1.models import pizzaShop
+from app1.models import Pizza
+# Register your models here.
+
+admin.site.register(pizzaShop)
+admin.site.register(Pizza)
---
+++ app1/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class pizzaShop(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Pizzeria')
+    description = models.TextField(verbose_name='Description')
+    raiting = models.FloatField(default=0, verbose_name='Raiting')
+    url = models.URLField(verbose_name='Internet address')
+
+    class Meta:
+        verbose_name='Pizzeria'
+        verbose_name_plural='Pizzerias'
+
+    def __str__(self):
+        return self.name
+
+class Pizza(models.Model):
+    pizzashopFK = models.ForeignKey(pizzaShop, on_delete=models.CASCADE)
+    #pizzashopFK2= models.ManyToManyField
+    #pizzashopFK3 = models.OneToOneField
+    name = models.CharField(max_length=30, verbose_name='Name of pizza')
+    sort_description = models.CharField(max_length=30, verbose_name='Short description')
+    price = models.IntegerField(default=0, verbose_name='Price of pizza')
+
+    class Meta:
+        verbose_name='Pizzeria'
+        verbose_name_plural='Pizzas'
+        ordering=['name']
+
+    def __str__(self):
+        return self.name
